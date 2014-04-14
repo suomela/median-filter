62b7d3c73becb28791ba96af763b55ec2a636c10
Jukka Suomela <jukka.suomela@iki.fi>
2014-04-14 18:20:36 +0300

    More extensive Matlab benchmarks (needed a lot more RAM).

--- other/matlab/run.m
+++ other/matlab/run.m
@@ -1,16 +1,12 @@
 f = fopen('result', 'w');
-mfbenchmark(f, 1, 10000);
-mfbenchmark(f, 2, 5000);
-mfbenchmark(f, 5, 2000);
-mfbenchmark(f, 10, 1000);
-mfbenchmark(f, 20, 500);
-mfbenchmark(f, 50, 200);
-mfbenchmark(f, 100, 100);
-mfbenchmark(f, 200, 50);
-mfbenchmark(f, 500, 20);
+mfbenchmark(f, 10000, 10);
+mfbenchmark(f, 1000, 100);
+mfbenchmark(f, 100, 1000);
+mfbenchmark(f, 10, 10000);
+mfbenchmark(f, 1, 100000);
 mfbenchmark(f, 1000, 10);
-mfbenchmark(f, 2000, 5);
-mfbenchmark(f, 5000, 2);
-mfbenchmark(f, 10000, 1);
+mfbenchmark(f, 100, 100);
+mfbenchmark(f, 10, 1000);
+mfbenchmark(f, 1, 10000);
 fclose(f);
 exit
