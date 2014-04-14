8a64fb8d5acc9d1a50f212e53db92ab8a90945f3
Jukka Suomela <jukka.suomela@iki.fi>
2014-04-14 16:28:22 +0300

    A bit more reliable Matlab benchmarks.

--- other/matlab/mfbenchmark.m
+++ other/matlab/mfbenchmark.m
@@ -1,14 +1,17 @@
 function mfbenchmark(f, h, b)
-%  mfbenchmark(h, b)   Benchmark medfilt1
+%  mfbenchmark(f, h, b)   Benchmark medfilt1
 %
 %    f = log file
 %    h = half-window size
 %    b = number of blocks
 
+seed = 1;
+randn('seed', seed);
 k = 2*h+1;
 n = k*b;
 x = randi([0,intmax('uint32')], n, 1);
+y = medfilt1(x, 1);  % make sure the function is already loaded
 tic;
 y = medfilt1(x, k);
 e = toc;
-fprintf(f, 'matlab\t%u\t%u\tr-large\t0\t%.5f\n', h, b, e);
+fprintf(f, 'matlab\t%u\t%u\tr-large\t%u\t%.5f\n', h, b, seed, e);
