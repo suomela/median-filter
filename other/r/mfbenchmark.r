9cf0f7908f8d3dd95a5c31e4847e63d43abd7d8c
Jukka Suomela <jukka.suomela@iki.fi>
2014-04-21 13:15:38 +0300

    R benchmarks, two different algorithms.

---
+++ other/r/mfbenchmark.r
@@ -0,0 +1,52 @@
+test <- function(h, b, alg) {
+    seed <- 1
+    set.seed(seed)
+    k <- 2*h+1
+    n <- k*b
+    x <- sample(2**32, n)
+    e <- system.time(runmed(x, k, "keep", alg))["elapsed"]
+    cat(sprintf('R%s\t%d\t%d\tr-large\t%d\t%.3f\n', alg, h, b, seed, e), sep="")
+    flush.console()
+}
+
+test1 <- function(h, b) {
+    test(h, b, "Turlach")
+}
+
+test2 <- function(h, b) {
+    test(h, b, "Stuetzle")
+}
+
+
+test1(1, 10000)
+test1(10, 1000)
+test1(100, 100)
+test1(1000, 10)
+test1(1, 100000)
+test1(10, 10000)
+test1(100, 1000)
+test1(1000, 100)
+test1(10000, 10)
+test1(1, 1000000)
+test1(10, 100000)
+test1(100, 10000)
+test1(1000, 1000)
+test1(10000, 100)
+test1(100000, 10)
+test1(1, 10000000)
+test1(10, 1000000)
+test1(100, 100000)
+test1(1000, 10000)
+test1(10000, 1000)
+test1(100000, 100)
+test1(1000000, 10)
+
+test2(1, 10000)
+test2(10, 1000)
+test2(100, 100)
+test2(1000, 10)
+test2(1, 100000)
+test2(10, 10000)
+test2(100, 1000)
+test2(1000, 100)
+test2(10000, 10)
---
+++ other/r/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+R --version | tee log || exit 1
+R CMD BATCH --slave --no-timing mfbenchmark.r /dev/stdout | tee result
