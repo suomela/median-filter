167807baf243facd294b9138cc373a9b02011663
Jukka Suomela <jukka.suomela@iki.fi>
2014-04-21 13:53:04 +0300

    More benchmarks for easier comparison with R.

--- other/comparison/mfbenchmark.sh
+++ other/comparison/mfbenchmark.sh
@@ -12,3 +12,18 @@ for alg in 0 1 2 3; do
     $tester 1000 100 $alg 4 1 || exit 1
     $tester 10000 10 $alg 4 1 || exit 1
 done
+for alg in 0 1 2; do
+    $tester 1 1000000 $alg 4 1 || exit 1
+    $tester 10 100000 $alg 4 1 || exit 1
+    $tester 100 10000 $alg 4 1 || exit 1
+    $tester 1000 1000 $alg 4 1 || exit 1
+    $tester 10000 100 $alg 4 1 || exit 1
+    $tester 100000 10 $alg 4 1 || exit 1
+    $tester 1 10000000 $alg 4 1 || exit 1
+    $tester 10 1000000 $alg 4 1 || exit 1
+    $tester 100 100000 $alg 4 1 || exit 1
+    $tester 1000 10000 $alg 4 1 || exit 1
+    $tester 10000 1000 $alg 4 1 || exit 1
+    $tester 100000 100 $alg 4 1 || exit 1
+    $tester 1000000 10 $alg 4 1 || exit 1
+done
