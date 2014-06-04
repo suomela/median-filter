69d4391671a76adc31afc33ea27aa208c55bd5af
Jukka Suomela <jukka.suomela@iki.fi>
2014-06-04 20:02:18 +0300

    Scripts for running experiments.

--- other/r/run.sh
+++ other/r/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 seed="$1"
+cat /proc/cpuinfo > "log-cpu-$seed"
+free > "log-memory-$seed"
 R --version | tee "log-$seed" || exit 1
 R CMD BATCH --slave --no-timing "--args seed=$seed" mfbenchmark.r /dev/stdout | tee "result-$seed"
