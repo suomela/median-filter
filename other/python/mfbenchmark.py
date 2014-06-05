b6b1208855bc0897f19f64b6a8d20c1307abda2a
Jukka Suomela <jukka.suomela@iki.fi>
2014-06-05 12:58:32 +0300

    New Python experiments on OS X.

--- other/python/mfbenchmark.py
+++ other/python/mfbenchmark.py
@@ -5,8 +5,7 @@ import scipy.signal
 import time
 import sys
 
-def test(h, b):
-    seed = 1
+def test(h, b, seed):
     numpy.random.seed(seed)
     k = 2*h+1
     n = k*b
@@ -17,12 +16,13 @@ def test(h, b):
     sys.stdout.write('python\t{}\t{}\tr-large\t{}\t{:.5f}\n'.format(h, b, seed, e))
     sys.stdout.flush()
 
-test(1, 10000)
-test(10, 1000)
-test(100, 100)
-test(1000, 10)
-test(1, 100000)
-test(10, 10000)
-test(100, 1000)
-test(1000, 100)
-test(10000, 10)
+for seed in range(1,11):
+    test(1, 10000, seed)
+    test(10, 1000, seed)
+    test(100, 100, seed)
+    test(1000, 10, seed)
+    test(1, 100000, seed)
+    test(10, 10000, seed)
+    test(100, 1000, seed)
+    test(1000, 100, seed)
+    test(10000, 10, seed)
