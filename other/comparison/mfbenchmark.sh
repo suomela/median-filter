c0fc357b5d87ae5ae50b4c77fc7ddff91e6b247e
Jukka Suomela <jukka.suomela@iki.fi>
2014-06-05 14:09:55 +0300

    New experiments on OS X.

--- other/comparison/mfbenchmark.sh
+++ other/comparison/mfbenchmark.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
 tester=../../build-gcc-short/tester
-for alg in 0 1 2 3; do
-    $tester 1 10000 $alg 4 1 || exit 1
-    $tester 10 1000 $alg 4 1 || exit 1
-    $tester 100 100 $alg 4 1 || exit 1
-    $tester 1000 10 $alg 4 1 || exit 1
-    $tester 1 100000 $alg 4 1 || exit 1
-    $tester 10 10000 $alg 4 1 || exit 1
-    $tester 100 1000 $alg 4 1 || exit 1
-    $tester 1000 100 $alg 4 1 || exit 1
-    $tester 10000 10 $alg 4 1 || exit 1
-done
-for alg in 0 1 2; do
-    $tester 1 1000000 $alg 4 1 || exit 1
-    $tester 10 100000 $alg 4 1 || exit 1
-    $tester 100 10000 $alg 4 1 || exit 1
-    $tester 1000 1000 $alg 4 1 || exit 1
-    $tester 10000 100 $alg 4 1 || exit 1
-    $tester 100000 10 $alg 4 1 || exit 1
-    $tester 1 10000000 $alg 4 1 || exit 1
-    $tester 10 1000000 $alg 4 1 || exit 1
-    $tester 100 100000 $alg 4 1 || exit 1
-    $tester 1000 10000 $alg 4 1 || exit 1
-    $tester 10000 1000 $alg 4 1 || exit 1
-    $tester 100000 100 $alg 4 1 || exit 1
-    $tester 1000000 10 $alg 4 1 || exit 1
+for seed in 1 2 3 4 5 6 7 8 9 10; do
+    for alg in 0 1 2 3; do
+        $tester 1 10000 $alg 4 $seed || exit 1
+        $tester 10 1000 $alg 4 $seed || exit 1
+        $tester 100 100 $alg 4 $seed || exit 1
+        $tester 1000 10 $alg 4 $seed || exit 1
+        $tester 1 100000 $alg 4 $seed || exit 1
+        $tester 10 10000 $alg 4 $seed || exit 1
+        $tester 100 1000 $alg 4 $seed || exit 1
+        $tester 1000 100 $alg 4 $seed || exit 1
+        $tester 10000 10 $alg 4 $seed || exit 1
+    done
+    for alg in 0 1 2; do
+        $tester 1 1000000 $alg 4 $seed || exit 1
+        $tester 10 100000 $alg 4 $seed || exit 1
+        $tester 100 10000 $alg 4 $seed || exit 1
+        $tester 1000 1000 $alg 4 $seed || exit 1
+        $tester 10000 100 $alg 4 $seed || exit 1
+        $tester 100000 10 $alg 4 $seed || exit 1
+        $tester 1 10000000 $alg 4 $seed || exit 1
+        $tester 10 1000000 $alg 4 $seed || exit 1
+        $tester 100 100000 $alg 4 $seed || exit 1
+        $tester 1000 10000 $alg 4 $seed || exit 1
+        $tester 10000 1000 $alg 4 $seed || exit 1
+        $tester 100000 100 $alg 4 $seed || exit 1
+        $tester 1000000 10 $alg 4 $seed || exit 1
+    done
 done
